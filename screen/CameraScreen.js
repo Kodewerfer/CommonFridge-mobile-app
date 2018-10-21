165ff420026d2a7e4cb0d9adf8657b6ea9abbcac
JiaLin Li <lijleen@gmail.com>
2018-10-21 00:07:59 -0400

    First commit

---
+++ screen/CameraScreen.js
@@ -0,0 +1,153 @@
+import React from 'react';
+import { Text, View, TouchableOpacity, StyleSheet, TouchableNativeFeedback } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Camera, Permissions } from 'expo';
+
+export class CameraScreen extends React.Component {
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back,
+    isAbleToSnap: true
+  };
+
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
+
+  async takeSnap() {
+    if (!this.camera || !this.state.isAbleToSnap) {
+      return;
+    }
+
+    let that = this
+
+    this.camera.takePictureAsync({
+      onPictureSaved: (photo) => {
+        console.log(photo)
+
+        that.setState({
+          isAbleToSnap: true
+        })
+      }
+
+    });
+
+    this.setState({
+      isAbleToSnap: false
+    })
+  }
+
+  onFlipCamera() {
+    this.setState({
+      type: this.state.type === Camera.Constants.Type.back
+        ? Camera.Constants.Type.front
+        : Camera.Constants.Type.back,
+    });
+  }
+
+  render() {
+
+    const { hasCameraPermission } = this.state;
+
+    const ableToSnap = this.state.isAbleToSnap;    
+
+    const snapBG = ableToSnap ? Styles.snapBtnBG : Styles.snapBtnBGDis;
+    const isAlertShows = ableToSnap ? { display: 'none' } : '';
+    const snapBtnOpacity = ableToSnap ? 0.2 : 1;
+
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+
+        <View style={Styles.fullScreen}>
+          <Camera ref={(ref) => this.camera = ref} style={Styles.fullScreen} type={this.state.type}>
+
+            <View
+              style={Styles.actionBar}>
+
+              <TouchableOpacity style={Styles.flipBtn}>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={Styles.snapBtn}
+                onPress={() => this.takeSnap()}
+                activeOpacity={snapBtnOpacity}
+              >
+                <Text style={[Styles.alertText, isAlertShows]}>Processing...</Text>
+                <View style={snapBG}>
+                  <Icon name="camera" style={Styles.snapIcon} size={40} color="#fff" />
+                </View>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={Styles.flipBtn}
+                onPress={() => this.onFlipCamera()}>
+                <Text
+                  style={Styles.flipBtnText}>
+                  Flip
+                </Text>
+              </TouchableOpacity>
+
+            </View>
+          </Camera>
+        </View >
+
+      );
+    }
+  }
+
+
+}
+
+
+const Styles = StyleSheet.create({
+  fullScreen: {
+    flex: 1
+  },
+  actionBar: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+  },
+  flipBtn: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  flipBtnText: {
+    fontSize: 20,
+    paddingBottom: 35,
+    paddingTop: 20,
+    color: 'white'
+  },
+  snapBtn: {
+    flex: 3,
+    paddingBottom: 40,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  snapBtnBG: {
+    backgroundColor: '#4F2D7F',
+    borderRadius: 50,
+  },
+  snapBtnBGDis: {
+    backgroundColor: '#ccc',
+    borderRadius: 50,
+  },
+  snapIcon: {
+    padding: 20,
+    color: 'white'
+  },
+  alertText: {
+    color: '#fff',
+    backgroundColor: '#4F2D7F',
+    padding: 20,
+    fontSize: 20,
+    marginBottom: 15,
+    alignSelf: 'center'
+  }
+});
---
+++ screen/WelcomeScreen.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native';
+
+export class WelcomeScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showNameInput: false,
+      username: null,
+      isNameEmpty: true
+    }
+
+    setTimeout(() => {
+      this.setState({
+        showNameInput: true
+      })
+    }, 2500);
+  }
+  handleInput(text) {
+    this.setState({
+      username: text
+    })
+  }
+  onSubmit() {
+    // debugger;
+  }
+  render() {
+
+    const titleText = "Bishop’s Community Fridge";
+    const secondLine = "Welcome!";
+    const inputTop = "You shall be known as";
+
+    const inputStyle = this.state.showNameInput ? styles.textInput : styles.invis;
+
+    return (
+
+      <View style={styles.container}>
+
+        <View style={styles.welcomeTexts}>
+          <Text style={styles.text}>{titleText}</Text>
+          <Text style={[styles.text, { fontSize: 30 }, { padding: 30 }]}>{secondLine}</Text>
+        </View>
+
+        <KeyboardAvoidingView behavior="padding" enabled style={inputStyle}>
+
+          <Text style={[styles.text, { fontFamily: 'space-mono' }]}>{inputTop}</Text>
+
+          <TextInput
+            style={styles.textbox}
+            textContentType={'givenName'}
+            onChangeText={(text) => this.handleInput(text)}
+            onSubmitEditing={() => this.onSubmit()}
+          />
+
+          <TouchableOpacity onPress={() => this.onSubmit()}>
+            <View style={styles.submitBtn}>
+              <Text style={{ color: '#fff', fontSize: 15 }}>Submit</Text>
+            </View>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
+      </View>
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#4F2D7F',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+  welcomeTexts: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textInput: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  textbox: {
+    fontFamily: 'space-mono',
+    textAlign: "center",
+    width: 300,
+    backgroundColor: '#fff',
+    fontSize: 20,
+    padding: 15,
+    marginTop: 30,
+    marginBottom: 30,
+    borderRadius: 10
+
+  },
+  submitBtn: {
+    borderRadius: 10,
+    paddingLeft: 100,
+    paddingRight: 100,
+    paddingTop: 20,
+    paddingBottom: 20,
+    backgroundColor: '#2ac12a',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 22,
+  },
+  invis: {
+    display: "none"
+  }
+});
