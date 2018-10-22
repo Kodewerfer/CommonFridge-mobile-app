72aea42da3c494a0bec590b1613b1c26b971aef2
JiaLin Li <lijleen@gmail.com>
2018-10-21 22:24:59 -0400

    Complete major UI, adding animation

--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { StyleSheet, Text, Image, View, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native';
+import { Dimensions, Animated, StyleSheet, Text, Image, View, Keyboard, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native';
 
 export class HomeScreen extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      isKeyboardUp: false,
     }
+  }
+
+  componentDidMount() {
+    this.keyboardWillShowSub = Keyboard.addListener('keyboardWillShow', this._keyboardDidShow);
+    this.keyboardWillHideSub = Keyboard.addListener('keyboardWillHide', this._keyboardDidHide);
 
   }
 
@@ -39,42 +45,132 @@ export class HomeScreen extends Component {
     )
   }
 
-  render() {
-
-    const secondLine = "Welcome! " + this.props.username;
-
-    return (
+  keyboardAccordanceRender() {
 
 
-      <View style={Styles.container}>
-
-        <View style={Styles.containerInner}>
+    if (!this.state.isKeyboardUp) {
+      return (
+        <View style={Styles.topWrapper}>
 
           <View style={Styles.containerTop}>
 
-            <Text style={[Styles.text, { fontSize: 30 }, { padding: 30 }]}>{secondLine}</Text>
+            <Text style={[Styles.text, { fontSize: 30 }]}>Welcome!</Text>
+            <Text style={[Styles.text, { fontSize: 30 }]}>{this.props.username}</Text>
 
           </View>
 
           <View style={Styles.containerMid}>
-            <TouchableOpacity onPress={() => this.props.toggleCamera()}>
-              {this.renderCenter()}
-            </TouchableOpacity>
+            {this.renderCenter()}
           </View>
-
-          <View style={Styles.containerBtm}>
-            <Text style={[Styles.text, { fontSize: 30 }, { padding: 30 }]}>{secondLine}</Text>
-          </View>
-
-
-
 
         </View>
+      );
+    }
 
+
+  }
+
+  render() {
+
+    const inputTop = 'Item description'
+
+    this.topWrapperOpacity = new Animated.Value(1);
+    this.keyboardHeight = new Animated.Value(0);
+    this.topWrapperHeight = new Animated.Value(this.getDimensions().topWH);
+    this.bottomWrapperHeight = new Animated.Value(this.getDimensions().btmWH);
+
+    return (
+      <View style={Styles.container}>
+        <Animated.View style={[Styles.containerInner, { paddingBottom: this.keyboardHeight, }]}>
+
+          <Animated.View style={[Styles.topWrapper, { height: this.topWrapperHeight }, { opacity: this.topWrapperOpacity }]}>
+
+            <View style={Styles.containerTop}>
+
+              <Text style={[Styles.text, { fontSize: 30 }]}>Welcome!</Text>
+              <Text style={[Styles.text, { fontSize: 30 }]}>{this.props.username}</Text>
+
+            </View>
+
+            <View style={Styles.containerMid}>
+              {this.renderCenter()}
+            </View>
+
+          </Animated.View>
+
+          <Animated.View style={[Styles.bottomWrapper, { height: this.bottomWrapperHeight }]}>
+
+            <View style={Styles.containerBtm}>
+              <Text style={Styles.text}>{inputTop}</Text>
+              <TextInput
+                style={Styles.textbox}
+                textContentType={'givenName'}
+                onChangeText={(text) => { }}
+                onSubmitEditing={() => { }}
+              />
+
+              <TouchableOpacity onPress={() => this.onSubmit()}>
+                <View style={Styles.submitBtn}>
+                  <Text style={{ color: '#fff', fontSize: 15 }}>Submit</Text>
+                </View>
+              </TouchableOpacity>
+
+            </View>
+          </Animated.View>
+
+        </Animated.View>
       </View>
-
     );
   }
+
+  _keyboardDidShow = (event) => {
+
+    Animated.parallel([
+      Animated.timing(this.keyboardHeight, {
+        duration: event.duration,
+        toValue: event.endCoordinates.height + 50,
+      }),
+      Animated.timing(this.topWrapperOpacity, {
+        duration: event.duration,
+        toValue: 0,
+      }),
+      Animated.timing(this.topWrapperHeight, {
+        duration: event.duration,
+        toValue: this.getDimensions().windowHeight / 4,
+      }),
+    ]).start();
+
+  }
+
+  _keyboardDidHide = (event) => {
+    Animated.parallel([
+      Animated.timing(this.keyboardHeight, {
+        duration: event.duration,
+        toValue: 0,
+      }),
+      Animated.timing(this.topWrapperOpacity, {
+        duration: event.duration,
+        toValue: 1,
+      }),
+      Animated.timing(this.topWrapperHeight, {
+        duration: event.duration,
+        toValue: this.getDimensions().topWH,
+      }),
+
+    ]).start();
+  }
+
+  getDimensions() {
+    const windowHeight = Dimensions.get('window').height;
+    const topWH = windowHeight / 1.6;
+
+    return {
+      windowHeight: windowHeight,
+      topWH: topWH,
+      btmWH: windowHeight - topWH
+    }
+  }
+
 
 }
 
@@ -83,6 +179,8 @@ const Styles = StyleSheet.create({
     alignSelf: 'center',
     color: '#616161',
     fontSize: 22,
+
+
   },
   tipsText: {
     alignSelf: 'center',
@@ -100,11 +198,17 @@ const Styles = StyleSheet.create({
   containerInner: {
     flex: 1,
     borderRadius: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
+    // height: '100%',
     backgroundColor: '#F8ECC2',
   },
+  topWrapper: {
+    height: "65%",
+  },
+  bottomWrapper: {
+    height: "35%"
+  },
   imgPort: {
+    borderRadius: 15,
     width: 205,
     height: 205
   },
@@ -117,18 +221,41 @@ const Styles = StyleSheet.create({
     borderStyle: "dashed",
     borderWidth: 2
   },
+  textbox: {
+    fontFamily: 'space-mono',
+    textAlign: "center",
+    width: 300,
+    backgroundColor: '#fff',
+    fontSize: 20,
+    padding: 15,
+    marginTop: 30,
+    marginBottom: 30,
+    borderRadius: 10
+
+  },
+  submitBtn: {
+    borderRadius: 10,
+    paddingLeft: 100,
+    paddingRight: 100,
+    paddingTop: 20,
+    paddingBottom: 20,
+    backgroundColor: '#2ac12a',
+  },
   containerTop: {
-    flex: 0.7,
+    flex: 0.6,
+    // height:'20%',
     alignItems: 'center',
     justifyContent: 'flex-end',
   },
   containerMid: {
-    flex: 2,
+    flex: 1.5,
+    // height:'60%',
     alignItems: 'center',
     justifyContent: 'center',
   },
   containerBtm: {
     flex: 1,
+    // height:'20%',
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
